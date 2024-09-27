fc7d4c7d8dd6bfb251df1a10a48fc4236093044d
Wellington <wellingtoncerqueira17@gmail.com>
2024-09-27 17:12:31 -0300

    Exception handling - update

--- src/main/java/com/educandoweb/course/services/UserServices.java
+++ src/main/java/com/educandoweb/course/services/UserServices.java
@@ -12,6 +12,8 @@ import com.educandoweb.course.repositories.UserRepository;
 import com.educandoweb.course.services.exceptions.DatabaseException;
 import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserServices {
 	
@@ -42,9 +44,14 @@ public class UserServices {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		}
+		catch(EntityNotFoundException e){
+			throw new ResourceNotFoundException(id);
+		}
 	}
 	
 	private void updateData(User entity, User obj) {
@@ -52,5 +59,5 @@ public class UserServices {
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());	
 	}
-	 
+
 }
